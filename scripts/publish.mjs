0601235e633c1ace0d5c378970fa5ba9ed453646
longo-andrea <longoandrea.dev@gmail.com>
2022-12-03 16:12:26 +0100

    docs: add publish script and changed packages name

---
+++ scripts/publish.mjs
@@ -0,0 +1,21 @@
+import * as fs from "node:fs/promises";
+import path from "path";
+import { execSync } from "child_process";
+import { fileURLToPath } from "url";
+
+// Replicate __dirname and __filename behavior
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const publishAllPackages = async function () {
+	const packages = await fs.readdir(path.join(__dirname, "../packages"));
+	for (const pkg of packages) {
+		execSync("npm publish --access public", {
+			stdio: "inherit",
+			cwd: path.join("packages", pkg, "dist"),
+		});
+	}
+};
+
+execSync("pnpm build", { stdio: "inherit" });
+publishAllPackages();
